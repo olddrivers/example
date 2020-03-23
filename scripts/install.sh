5ff8c7079c2bb57d05f7093869845afcb90e4ada
qilin <qilin@big.one>
2020-03-23 18:45:09 +0800

    test install

---
+++ scripts/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+version="0.12.24"
+
+function terraform-install() {
+  [[ -f /usr/local/bin/terraform ]] && echo "`/usr/local/bin/terraform version` already installed at /usr/local/bin/terraform" && return 0
+  OS=$(uname -s)
+  #LATEST_VERSION=$(curl -sL https://releases.hashicorp.com/terraform/index.json | jq -r '.versions[].version' | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | egrep -v 'alpha|beta|rc' | tail -1)
+  LATEST_URL="https://releases.hashicorp.com/terraform/${version}/terraform_${version}_${OS,,}_amd64.zip"
+  curl ${LATEST_URL} > /tmp/terraform_${version}_${OS,,}_amd64.zip
+  mkdir -p /usr/local/bin
+  (cd /usr/local/bin && unzip /tmp/terraform_${version}_${OS,,}_amd64.zip)
+  /usr/local/bin/terraform version
+}
+
+terraform-install
