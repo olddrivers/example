9d37265692e75df2ebda02c87540b765b293b0b5
qilin <qilin@big.one>
2020-03-24 14:39:46 +0800

    test install

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,13 +5,18 @@ version="0.12.24"
 #LATEST_VERSION=$(curl -sL https://releases.hashicorp.com/terraform/index.json | jq -r '.versions[].version' | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | egrep -v 'alpha|beta|rc' | tail -1)
 
 function  check_exist(){
-  #if [  /usr/local/bin/terraform version | grep ${version} ] &&  [ -f /usr/local/bin/terraform ]; then
-   /usr/local/bin/terraform version | grep ${version} && exit 0 || [[ -f /usr/local/bin/terraform ]] && rm -rf /usr/local/bin/terraform
+	$(which terraform) version | grep -q "${version}" 
+    if [ $? -eq 0 ]; then
+        echo "$($(which terraform) version) already installed"
+        exit 0
+    else
+      [[ -f /usr/local/bin/terraform ]] && rm -rf $(which terraform)
+    fi
 }
 
 
 function terraform-install() {
-  [[ -f /usr/local/bin/terraform ]] && echo "`/usr/local/bin/terraform version` already installed at /usr/local/bin/terraform" && return 0
+  #[[ -f /usr/local/bin/terraform ]] && echo "`/usr/local/bin/terraform version` already installed at /usr/local/bin/terraform" && return 0
   OS=$(uname -s)
   LATEST_URL="https://releases.hashicorp.com/terraform/${version}/terraform_${version}_${OS,,}_amd64.zip"
   curl ${LATEST_URL} > /tmp/terraform_${version}_${OS,,}_amd64.zip
